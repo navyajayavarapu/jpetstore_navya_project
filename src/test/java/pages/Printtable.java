abc0b4ace94a8c8837e07b621b8929dfa05ff645
navyajayavarapu <60091934+navyajayavarapu@users.noreply.github.com>
2020-03-27 16:46:30 +0530

    Update Printtable.java

--- src/test/java/pages/Printtable.java
+++ src/test/java/pages/Printtable.java
@@ -16,45 +16,21 @@ public class Printtable
 	
 	// to launch the browser
 	
-			public void browserLaunch(String browser,String url)
-			{
-				try {
-							//To launch firefox Browser
-					if (browser.equalsIgnoreCase("firefox")) {
-						System.setProperty("webdriver.gecko.driver","C:\\Users\\Personal\\Desktop\\navya-testcase-master\\src\\test\\resources\\drivers\\geckodriver.exe");
-						driver = new FirefoxDriver();
-					}
-							// To launch chrome Browser
-					else if (browser.equalsIgnoreCase("chrome")) 
-					{
-						System.setProperty("webdriver.chrome.driver","C:\\Users\\Personal\\Desktop\\navya-testcase-master\\src\\test\\resources\\drivers\\chromedriver.exe");
-						driver=new ChromeDriver();
-					}
-							// To launch InternetExplorer
-					else if (browser.equalsIgnoreCase("Internet Explorer")) {
-							System.setProperty("webdriver.ie.driver","D:\\Downloads\\IEDriverServer_Win32_3.150.1\\IEDriverServer.exe");
-						driver=new InternetExplorerDriver();  
-					}
-							//to maximize the window
-					driver.manage().window().maximize(); 
-					driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-					driver.get(url);
-				
-				} 
-				
-				//if browser cannot be launched
-				catch (WebDriverException e) 
-				{
-					System.out.println("Browser could not be launched");
-				}
-			}
-	
-	public void url() {
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Personal\\Desktop\\navya-testcase-master\\src\\test\\resources\\drivers\\chromedriver.exe");
+		public void url(String browser) 
+		{
+		if(browser.equalsIgnoreCase("chrome"))
+		{
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\pooja\\Desktop\\navya\\Selenium_jar_files\\drivers\\chromedriver.exe");
 		driver = new ChromeDriver();
+		}
+		else if(browser.equalsIgnoreCase("firefox"))
+		{
+			System.setProperty("webdriver.gecko.driver", "C:\\Users\\pooja\\Desktop\\navya\\Selenium_jar_files\\drivers\\geckodriver.exe");
+			driver = new FirefoxDriver();
+		}
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
-		driver.get("https://petstore.octoperf.com/actions/Catalog.action");
+		driver.get("https://petstore.octoperf.com/actions/Catalog.action?viewProduct=&productId=K9-BD-01");
 		System.out.println(driver.getTitle());
 	
 
